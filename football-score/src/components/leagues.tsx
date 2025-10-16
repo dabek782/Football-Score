ef0da5d1dea726fda20d7c2599558968dc48b9ad
dabek782 <kubadabrowski782@gmail.com>
2025-10-16 23:46:25 +0200

    Added Leagues tab with 5 top football leagues and champions league tab also added arrow for future leagues

--- football-score/src/components/leagues.tsx
+++ football-score/src/components/leagues.tsx
@@ -1,1 +1,14 @@
 import "./leagues.css"
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+function Leagues(){
+    return(
+        <div className="selector">
+        <button className="England"> <span className="fi fi-gb-eng"></span>Premier League </button>
+        <button className="Italy"><span className="fi fi-it"></span>Serie A</button>
+        <button className="Germany"> <span className="fi fi-de"></span>Bundesliga</button>
+        <button className="Spain"><span className="fi fi-es"></span>La liga</button>
+        <button className="France"><span className="fi fi-fr"></span>Ligue 1</button>
+        </div>
+    )
+}
+export default Leagues
